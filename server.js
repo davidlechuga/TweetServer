076c3e4ef2bfed6cd3268dbd5565808d5efaf697
davidlechuga <david.lechuga@lakmisystems.com.mx>
2019-11-01 14:27:36 -0600

    chages

--- server.js
+++ server.js
@@ -7,12 +7,69 @@ app.use(require('cors')());
 app.use(require('body-parser').json());
 
 
+
+app.get('/resumenes', async (req, res) => {
+
+
+  const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
+  const { IamAuthenticator } = require('ibm-watson/auth');
+
+  const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
+  version: '2019-07-12',
+  authenticator: new IamAuthenticator({
+    apikey: process.env.API_KEY,
+  }),
+  url: process.env.URL,
+});
+
+const analyzeParams = {
+  'url': 'https://twiitgov.mybluemix.net/search/AMLO',
+  'features': {
+    'keywords': {
+      'sentiment': true,
+      'emotion': true,
+      'limit': 3
+    }
+  }
+};
+
+naturalLanguageUnderstanding.analyze(analyzeParams)
+  .then(analysisResults => {
+   const data = analysisResults
+
+  console.log(data);
+
+  //  for(let i = 0 ; i < data.result.keywords.length; i++) {
+  //    console.log(data.result.keywords[i]);
+  //    console.log(`aqui esta el arreglo ${data.result.keywords[i]}`);
+
+  //  }
+  //  console.log(data)
+  
+
+   res.json ({
+    success: true,
+    message: 'ibm analyzer',
+    payload: data.result.keywords
+
+  })
+
+
+  })
+  .catch(err => {
+    console.log('error:', err);
+  });
+  
+})
+
+
+
 app.get('/tweets',async (req,res) => {
 
     const params = {count:1, tweet_mode: 'extended' };
     const dataTweets = await config.apiClient.get('statuses/home_timeline', params)
     .then((data)=>{
-        console.log(data)
+        console.log(`aqui esta la : ${data}`)
         return data;
     }
     )
@@ -27,9 +84,12 @@ app.get('/tweets',async (req,res) => {
 
     })
 
-    const {tweets} = payload;
-    const {data} = tweets;
-    console.log(data)
+    
+    // const {tweets} = payload;
+    // const {data} = tweets;
+    //  console.log(tweets)
+
+  
 
     // .then(timeline => {
     //     console.log(timeline.data)
@@ -58,6 +118,126 @@ app.get('/search/:word',(req,res) => {
 
 
 
+/* ::::::::  SENTIMIENTO IBM ::::::::.  */ 
+
+
+// const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
+// const { IamAuthenticator } = require('ibm-watson/auth');
+
+// const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
+//   version: '2019-07-12',
+//   authenticator: new IamAuthenticator({
+//     apikey: process.env.API_KEY,
+//   }),
+//   url: process.env.URL
+// });
+
+// const analyzeParams = {
+//   'url': 'https://twiitgov.mybluemix.net/search/AMLO',
+//   'features': {
+//     'sentiment': {
+//       'targets': [
+//         'data'
+//       ]
+//     }
+//   }
+// };
+
+
+
+// naturalLanguageUnderstanding.analyze(analyzeParams)
+//   .then(analysisResults => {
+//     console.log(JSON.stringify(analysisResults, null, 2));
+//   })
+//   .catch(err => {
+//     console.log('error:', err);
+//   });
+
+
+
+
+
+
+
+
+/* ::::::::  EMOCIONES IBM ::::::::.  */ 
+
+// const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
+// const { IamAuthenticator } = require('ibm-watson/auth');
+
+// const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
+//   version: '2019-07-12',
+//   authenticator: new IamAuthenticator({
+//     apikey: process.env.API_KEY
+//   }),
+//   url: process.env.URL
+// });
+
+// const analyzeParams = {
+//   'url': 'https://twiitgov.mybluemix.net/search/Trump',
+//     'features': {
+//     'emotion': {
+//       'targets': [
+//         'data'
+//       ]
+//     }
+//   }
+// };
+
+// naturalLanguageUnderstanding.analyze(analyzeParams)
+//   .then(analysisResults => {
+//     console.log(JSON.stringify(analysisResults, null, 2));
+//   })
+//   .catch(err => {
+//     console.log('error:', err);
+//   });
+
+
+
+
+
+
+
+/* ::::::::  ENTIDADES IBM ::::::::.  */ 
+
+// const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
+// const { IamAuthenticator } = require('ibm-watson/auth');
+
+// const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
+//   version: '2019-07-12',
+//   authenticator: new IamAuthenticator({
+//     apikey: process.env.API_KEY,
+//   }),
+//   url: process.env.URL,
+// });
+
+// const analyzeParams = {
+//   'url': 'https://twiitgov.mybluemix.net/search/Trump',
+//   'features': {
+//     'entities': {
+//       'sentiment': true,
+//       'limit': 1
+//     }
+//   }
+// };
+
+// naturalLanguageUnderstanding.analyze(analyzeParams)
+//   .then(analysisResults => {
+//     console.log(JSON.stringify(analysisResults, null, 2));
+//   })
+//   .catch(err => {
+//     console.log('error:', err);
+//   });
+
+
+
+
+
+
+
+/* ::::::::  PALABRAS CLAVE IBM ::::::::.  */ 
+
+
 const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
 const { IamAuthenticator } = require('ibm-watson/auth');
 
@@ -66,36 +246,36 @@ const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
   authenticator: new IamAuthenticator({
     apikey: process.env.API_KEY,
   }),
-  url: process.env.URL
+  url: process.env.URL,
 });
 
 const analyzeParams = {
-  'url': 'https://twiitgov.mybluemix.net/search/Trump',
+  'url': 'https://twiitgov.mybluemix.net/search/AMLO',
   'features': {
-    'sentiment': {
-      'targets': [
-        'data'
-      ]
+    'keywords': {
+      'sentiment': true,
+      'emotion': true,
+      'limit': 3
     }
   }
 };
 
 naturalLanguageUnderstanding.analyze(analyzeParams)
   .then(analysisResults => {
-    console.log(JSON.stringify(analysisResults, null, 2));
+   const data= analysisResults
+  //  console.log(data)
+   const {result} = data
+    dataIBM = data
+   console.log(result)
   })
   .catch(err => {
     console.log('error:', err);
   });
 
 
-
-
-
-
 app.listen(config.port,() => {
 
-    console.log(`runing on port ${config.port}`);
+   console.log(`runing on port ${config.port}`);
     
 
 });
